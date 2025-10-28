66f0ec0df702b305ede63a6e19e3b0decc3da23f
barambur26 <barambur@purdue.edu>
2025-10-28 19:42:36 -0400

    minor fixes to handler, created template.yaml for AWS SAM template file

--- lambda/handler.py
+++ lambda/handler.py
@@ -7,10 +7,3 @@ app = create_app()
 # Initialize the Mangum handler, which translates API Gateway events
 # into ASGI (FastAPI) requests and responses.
 handler = Mangum(app)
-
-
-
-
-# basic local test handler for debugging:
-if __name__ == "__main__":
-    print("This file is intended to be run by AWS Lambda/Mangum.")
